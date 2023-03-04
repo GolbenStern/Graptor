e5beb5d4d31b8723558a85c09ea04a59f8785468
SailorPolaris <chrisj1252@gmail.com>
2023-03-03 23:30:00 -0500

    This adds the Soleniod test code

---
+++ soleniodTest.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.OUT)
+
+while (True):
+    GPIO.output(18, 1)
+    sleep(1)
+    GPIO.output(18, 0)
+    sleep(1)
