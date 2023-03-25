eb49621afeab541341c86a8f10e8ec74d3c3a392
GolbenStern <sebasdiazmora@gmail.com>
2023-03-25 10:32:23 -0400

    servo update
    update

--- Servo_input.py
+++ Servo_input.py
@@ -1,39 +1,29 @@
-import pigpio
-import time
+import RPi.GPIO as GPIO
 
-# Set up pigpio and servo pin
-pi = pigpio.pi()
-servo_pin = 18
-pi.set_mode(servo_pin, pigpio.OUTPUT)
+# Set up GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.OUT)
 
-# Define servo parameters
-servo_min = 500
-servo_max = 2500
-servo_mid = int((servo_max - servo_min) / 2) + servo_min
-servo_current = servo_mid
+# Set up PWM
+pwm = GPIO.PWM(18, 50)  # 50 Hz frequency
+pwm.start(0)  # Start with 0% duty cycle
 
-# Define servo movement functions
-def servo_rotate_clockwise():
-    global servo_current
-    if servo_current < servo_max:
-        servo_current += 50
-    pi.set_servo_pulsewidth(servo_pin, servo_current)
-    time.sleep(0.02)
+try:
+    while True:
+        # Get user input without waiting for enter key
+        char = input()
+        
+        # Check user input and move servo accordingly
+        if char == 'q':
+            pwm.ChangeDutyCycle(2.5)  # 0 degrees
+        elif char == 'e':
+            pwm.ChangeDutyCycle(12.5)  # 180 degrees
+        else:
+            pwm.ChangeDutyCycle(7.5)  # 90 degrees
+        
+except KeyboardInterrupt:
+    pass
 
-def servo_rotate_counter_clockwise():
-    global servo_current
-    if servo_current > servo_min:
-        servo_current -= 50
-    pi.set_servo_pulsewidth(servo_pin, servo_current)
-    time.sleep(0.02)
-
-while True:
-    # Wait for user input
-    user_input = input("Press r to turn servo clockwise, or l to turn it counter-clockwise: ")
-    
-    if user_input == "r":
-        # Rotate servo clockwise
-        servo_rotate_clockwise()
-    elif user_input == "l":
-        # Rotate servo counter-clockwise
-        servo_rotate_counter_clockwise()
+# Clean up GPIO
+pwm.stop()
+GPIO.cleanup()
