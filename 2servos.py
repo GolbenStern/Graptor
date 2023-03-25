be9cefb2078a80ddf357bde469b37ed3f012b407
GolbenStern <sebasdiazmora@gmail.com>
2023-03-25 11:38:26 -0400

    Update 2servos.py
    update

--- 2servos.py
+++ 2servos.py
@@ -2,53 +2,80 @@
 
 import RPi.GPIO as GPIO
 import time
+import tkinter as tk
 
-# Set up GPIO and servo pins
 GPIO.setmode(GPIO.BCM)
-servo_pin_1 = 18
-servo_pin_2 = 19
-GPIO.setup(servo_pin_1, GPIO.OUT)
-GPIO.setup(servo_pin_2, GPIO.OUT)
 
-# Define servo parameters
-servo_min = 500
-servo_max = 2500
-servo_mid = int((servo_max - servo_min) / 2) + servo_min
-servo_current_1 = servo_mid
-servo_current_2 = servo_mid
+# define servo pins
+servo1_pin = 18
+servo2_pin = 17
 
-# Define servo movement functions
-def servo_rotate_clockwise(servo_pin, servo_current):
-    if servo_current < servo_max:
-        servo_current += 50
-    GPIO.output(servo_pin, GPIO.HIGH)
-    time.sleep(servo_current / 1000000.0)
-    GPIO.output(servo_pin, GPIO.LOW)
-    time.sleep(0.02 - servo_current / 1000000.0)
-    return servo_current
+# setup servo pins as output
+GPIO.setup(servo1_pin, GPIO.OUT)
+GPIO.setup(servo2_pin, GPIO.OUT)
 
-def servo_rotate_counter_clockwise(servo_pin, servo_current):
-    if servo_current > servo_min:
-        servo_current -= 50
-    GPIO.output(servo_pin, GPIO.HIGH)
-    time.sleep(servo_current / 1000000.0)
-    GPIO.output(servo_pin, GPIO.LOW)
-    time.sleep(0.02 - servo_current / 1000000.0)
-    return servo_current
+# create servo objects
+servo1 = GPIO.PWM(servo1_pin, 50)
+servo2 = GPIO.PWM(servo2_pin, 50)
 
-while True:
-    # Wait for user input
-    user_input = input("Press r to turn servo 1 clockwise, or l to turn it counter-clockwise. Press d to turn servo 2 clockwise, or m to turn it counter-clockwise: ")
-    
-    if user_input == "r":
-        # Rotate servo 1 clockwise
-        servo_current_1 = servo_rotate_clockwise(servo_pin_1, servo_current_1)
-    elif user_input == "l":
-        # Rotate servo 1 counter-clockwise
-        servo_current_1 = servo_rotate_counter_clockwise(servo_pin_1, servo_current_1)
-    elif user_input == "d":
-        # Rotate servo 2 clockwise
-        servo_current_2 = servo_rotate_clockwise(servo_pin_2, servo_current_2)
-    elif user_input == "m":
-        # Rotate servo 2 counter-clockwise
-        servo_current_2 = servo_rotate_counter_clockwise(servo_pin_2, servo_current_2)
+# start servos at 0 position
+servo1.start(0)
+servo2.start(0)
+
+# create GUI window
+root = tk.Tk()
+root.title("Servo Control")
+
+# function to run servo1 clockwise
+def run_servo1_cw():
+    servo1.ChangeDutyCycle(7.5) # turn to 90 degrees
+    time.sleep(1)
+    servo1.ChangeDutyCycle(12.5) # turn to 180 degrees
+    time.sleep(1)
+    servo1.ChangeDutyCycle(2.5) # turn to 0 degrees
+    time.sleep(1)
+
+# function to run servo1 counter-clockwise
+def run_servo1_ccw():
+    servo1.ChangeDutyCycle(12.5) # turn to 180 degrees
+    time.sleep(1)
+    servo1.ChangeDutyCycle(7.5) # turn to 90 degrees
+    time.sleep(1)
+    servo1.ChangeDutyCycle(2.5) # turn to 0 degrees
+    time.sleep(1)
+
+# function to stop servo1
+def stop_servo1():
+    servo1.ChangeDutyCycle(0)
+
+# function to run servo2 clockwise
+def run_servo2_cw():
+    servo2.ChangeDutyCycle(7.5) # turn to 90 degrees
+    time.sleep(1)
+    servo2.ChangeDutyCycle(12.5) # turn to 180 degrees
+    time.sleep(1)
+    servo2.ChangeDutyCycle(2.5) # turn to 0 degrees
+    time.sleep(1)
+
+# function to run servo2 counter-clockwise
+def run_servo2_ccw():
+    servo2.ChangeDutyCycle(12.5) # turn to 180 degrees
+    time.sleep(1)
+    servo2.ChangeDutyCycle(7.5) # turn to 90 degrees
+    time.sleep(1)
+    servo2.ChangeDutyCycle(2.5) # turn to 0 degrees
+    time.sleep(1)
+
+# function to stop servo2
+def stop_servo2():
+    servo2.ChangeDutyCycle(0)
+
+# create GUI buttons
+btn_servo1_cw = tk.Button(root, text="Servo1 CW", bg="green", command=run_servo1_cw)
+btn_servo1_ccw = tk.Button(root, text="Servo1 CCW", bg="red", command=run_servo1_ccw)
+btn_servo1_stop = tk.Button(root, text="Servo1 Stop", bg="gray", command=stop_servo1)
+btn_servo2_cw = tk.Button(root, text="Servo2 CW", bg="green", command=run_servo2_cw)
+btn_servo2_ccw = tk.Button(root, text="Servo2 CCW", bg="red", command=run_servo2_ccw)
+btn_servo2_stop = tk.Button(root, text="Servo2 Stop", bg="gray", command=stop_servo2)
+
+#
