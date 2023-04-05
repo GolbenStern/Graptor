a51c418074f15aac485a483b627a93cb0ab37d3f
SailorPolaris <chrisj1252@gmail.com>
2023-04-05 13:48:13 -0400

    This should be a good basis for a test

--- pressureSensor.py
+++ pressureSensor.py
@@ -1,11 +1,22 @@
-from hx711 import HX711
 import RPi.GPIO as GPIO
+from hx711 import HX711
 
+DT = 18
+SCK = 17
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(DT, GPIO.IN)
+GPIO.setup(SCK, GPIO.OUT)
 
-# Set the HX711 pins on the microcontroller
-hx = HX711(dout_pin=18, pd_sck_pin=17)
+hx = HX711(DT, SCK)
+hx.set_reading_format("MSB", "MSB")
+hx.set_reference_unit(1)
+hx.reset()
+hx.tare()
 
 while True:
-    reading = hx.get_raw_data_mean()
-    print(reading)
+    try:
+        val = hx.get_weight()
+        print(val)
+    except (KeyboardInterrupt, SystemExit):
+        GPIO.cleanup()
+        raise
