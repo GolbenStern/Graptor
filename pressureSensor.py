a46f4c68f4e2e4c3fd406d2f32448d0c9cd34e5e
SailorPolaris <chrisj1252@gmail.com>
2023-04-05 13:33:16 -0400

    Modified

--- pressureSensor.py
+++ pressureSensor.py
@@ -1,13 +1,11 @@
 from hx711 import HX711
 import RPi.GPIO as GPIO
 
+GPIO.setmode(GPIO.BCM)
+
 # Set the HX711 pins on the microcontroller
 hx = HX711(dout_pin=18, pd_sck_pin=17)
 
-# Calibrate the sensor and set the scale ratio
-hx.set_reference_unit(1) # 1 gram
-hx.tare()
-
-# Read the weight data from the sensor
-weight = hx.get_weight()
-print("Weight: {} grams".format(weight))
+while True:
+    reading = hx.get_raw_data_mean()
+    print(reading)
