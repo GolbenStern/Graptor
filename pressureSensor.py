6dcf84deda17aed63e18b1f6cad1f5e573634335
SailorPolaris <chrisj1252@gmail.com>
2023-04-05 13:26:05 -0400

    Modified

--- pressureSensor.py
+++ pressureSensor.py
@@ -1,7 +1,8 @@
 from hx711 import HX711
+import RPi.GPIO as GPIO
 
 # Set the HX711 pins on the microcontroller
-hx = HX711(dout_pin=5, pd_sck_pin=6)
+hx = HX711(dout_pin=18, pd_sck_pin=17)
 
 # Calibrate the sensor and set the scale ratio
 hx.set_reference_unit(1) # 1 gram
